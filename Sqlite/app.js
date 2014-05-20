c2447a5bd817cc8996123c735f1fa11875cdf4db
Abhijeet <void.aby@gmail.com>
2014-05-20 23:16:01 +0530

    added insert statement to sqlite

---
+++ Sqlite/app.js
@@ -0,0 +1,24 @@
+
+var fs = require("fs");
+var file = "test.db";
+var exists = fs.existsSync(file);
+
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(file);
+
+db.serialize(function(){
+
+// Creating table
+if(!exists){
+	db.run("CREATE TABLE STUFF (thing TEXT)");
+}
+
+
+// Statement
+var stmt = db.prepare("INSERT INTO STUFF VALUES(?)");
+	for(var i=0;i<10;i++)
+		stmt.run("Thing #"+i);
+stmt.finalize();
+});
+
+db.close();
