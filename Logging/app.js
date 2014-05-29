cdc34c0b6a65c1a960af2380f12bd2d581117413
Abhijeet <void.aby@gmail.com>
2014-05-29 23:56:33 +0530

    added code to logging

---
+++ Logging/app.js
@@ -0,0 +1,2 @@
+
+var log4js = require('log4js'); var logger = log4js.getLogger(); logger.info('Application is running'); logger.warn('Module cannot be loaded'); logger.error('Saved data was error'); logger.fatal('Server could not process'); logger.debug("Some debug messages");
