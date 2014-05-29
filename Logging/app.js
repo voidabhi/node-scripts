c904bb55318195679c3258cb88dd593e1749fdce
Abhijeet <void.aby@gmail.com>
2014-05-30 00:04:30 +0530

    added comments and fixed #18

--- Logging/app.js
+++ Logging/app.js
@@ -1,2 +1,16 @@
 
-var log4js = require('log4js'); var logger = log4js.getLogger(); logger.info('Application is running'); logger.warn('Module cannot be loaded'); logger.error('Saved data was error'); logger.fatal('Server could not process'); logger.debug("Some debug messages");
+// Importing logging module
+var log4js = require('log4js'); 
+// Getting logger object
+var logger = log4js.getLogger();
+ 
+// Logging the info
+logger.info('Application is running'); 
+// Logging the warning
+logger.warn('Module cannot be loaded'); 
+// Logging the error
+logger.error('Saved data was error');
+// Logging fatal error
+logger.fatal('Server could not process'); 
+// Logging debug line
+logger.debug("Some debug messages");
