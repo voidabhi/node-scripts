03717f4507bd9ecd9983849efafc60e809ae8be6
Abhijeet <void.aby@gmail.com>
2014-06-01 23:44:47 +0530

    added code for event handling

--- Event/app.js
+++ Event/app.js
@@ -3,3 +3,18 @@
 var EventEmitter = require('events').EventEmitter;
  var myEmitter = new EventEmitter;
 
+var connection = function(id){
+
+ // do something
+ console.log('client id: ' + id); 
+ 
+ }; 
+ 
+ myEmitter.on('connection', connection); 
+ 
+ myEmitter.on('message', function(msg){
+
+ // do something 
+ console.log('message: ' + msg);
+
+ });
