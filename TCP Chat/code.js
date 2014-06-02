4a500927732483d75e35538e8c1589d6d4045ad0
Abhijeet <void.aby@gmail.com>
2014-06-02 23:44:25 +0530

    registering chat server to client

--- TCP Chat/code.js
+++ TCP Chat/code.js
@@ -9,3 +9,29 @@ var chatServer  = net.createServer();
 // Client list
 var clientList = [];
 
+// Registering chatserver to client's connection event
+chatServer.on("connection",function(client){
+	client.name = client.remoteAddress+":"+client.remotePort;
+	client.write("Hi "+client.name+"!\n");
+	// When connected client sends a datapacket
+	
+	// Pushing new client to the list
+	clientList.push(client);
+	
+	// Sending data sent by any client to all clients
+	client.on("data",function(data){
+		broadcast(data,client);
+	});	
+	
+	// Removing clients from the list that have ended connection 
+	client.on("end",function(client){
+		contactList.splice(clientList.indexOf(client),1);
+	});
+	
+	client.on("error",function(e){
+		console.log(e);
+	});
+	//client.end();
+
+});
+
