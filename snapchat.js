2dc7b31155e8404450ecac4222e3b39fdf7a8128
Abhijeet Mohan <void.aby@gmail.com>
2016-12-03 23:57:55 +0530

    Create snapchat.js

---
+++ snapchat.js
@@ -0,0 +1,16 @@
+var snapchat = require('node-snapchat'),
+    fs = require('fs'),
+    username = "username",
+    password = "password";
+
+var client = new snapchat.Client({ username: username, password: password });
+
+client.on('loggedin', function(){
+    var timestamp = String(Math.round(new Date().getTime() / 1000));
+    client.getSnaps(timestamp, function(result){
+        var snap = result[0];
+        var stream = fs.createWriteStream('./' + snap.id + '.jpg', { flags: 'w', encoding: null, mode: 0666 });
+
+        client.getBlob(snap.id, stream, function() { } );
+    });
+});
