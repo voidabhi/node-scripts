121159e26a94a5677d66e85900818e5e8764328a
Abhijeet Mohan <void.aby@gmail.com>
2017-02-13 23:59:16 +0530

    Add mp3 server

---
+++ mp3-server.js
@@ -0,0 +1,18 @@
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path')
+    util = require('util');
+
+http.createServer(function(request, response) {
+    var filePath = 'path_to_file.mp3';
+    var stat = fileSystem.statSync(filePath);
+
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    util.pump(readStream, response);
+})
+.listen(2000);
