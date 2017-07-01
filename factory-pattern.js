a70d795342834449a4fafd18e40f9e076ec0ca2c
Abhijeet Mohan <void.aby@gmail.com>
2017-07-01 21:24:49 +0530

    Add factory pattern

---
+++ factory-pattern.js
@@ -0,0 +1,42 @@
+function CarMaker() { }
+
+CarMaker.prototype.drive = function () {
+  console.log( "Vroom, I have " + this.doors + " doors");
+};
+
+CarMaker.factory = function(type) {
+  var constr = type,
+      newcar;
+
+  if(typeof CarMaker[constr] !== "function") {
+    throw {
+      name: "Error",
+      message: constr + " doesn't exist"
+    };
+  }
+
+  if(typeof CarMaker[constr].prototype.drive !== "function") {
+    CarMaker[constr].prototype = new CarMaker();
+  }
+
+  newcar = new CarMaker[constr]();
+
+  return newcar;
+};
+
+CarMaker.Compact = function() {
+  this.doors = 4;
+};
+CarMaker.Convertible = function() {
+  this.doors = 2;
+};
+CarMaker.SUV = function() {
+  this.doors = 24;
+};
+
+var corolla = CarMaker.factory('Compact');
+var solstice = CarMaker.factory('Convertible');
+var cherokee = CarMaker.factory('SUV');
+corolla.drive();
+solstice.drive();
+cherokee.drive();
